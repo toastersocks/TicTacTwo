0cf3f3aa9a26ef3720a150f0b3ab702cf2a00dd1
James Pamplona <pamplonajames@gmail.com>
2015-03-04 17:53:29 -0800

    Disabled a couple of the default unused tests

--- TicTacTwo!Tests/TicTacTwo_Tests.swift
+++ TicTacTwo!Tests/TicTacTwo_Tests.swift
@@ -90,12 +90,12 @@ class TicTacTwo_Tests: XCTestCase {
         
     }
     
-    func testExample() {
+    func DISABLEtestExample() {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
     }
     
-    func testPerformanceExample() {
+    func DISABLEtestPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
             // Put the code you want to measure the time of here.
