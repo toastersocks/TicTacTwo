4885be1a1705c8947481f1bb84f014f611076387
James Pamplona <pamplonajames@gmail.com>
2015-03-06 15:19:08 -0800

    Changed from struct to a class. Added a method to get the game status as a human-readable string. Made currentPlayer a computed property and refactored the makeMove functions

--- TicTacTwo!/GameSession.swift
+++ TicTacTwo!/GameSession.swift
@@ -16,45 +16,117 @@ Represents the type of session being played
 - Remote:       A game between two players on different devices
 - SinglePlayer: A local single player game (most likely between a player and an AI)
 */
-enum SessionType {
-    case Local
-    case Remote
-    case SinglePlayer
+enum SessionType: String {
+    case Local = "Local"
+    case Remote = "Remote"
+    case SinglePlayer = "Single Player"
 }
 
 /**
 Represents a play session of a game
 */
-struct GameSession {
+class GameSession {
     
     let sessionType: SessionType
     let player: Player
     let opponent: Player
-    let game: Game
+    var game: Game
     let id: String
+    var currentPlayer: Player {
+        get {
+           return game.currentPlayer == player.playerPiece ? player : opponent
+        }
+    }
+    var lastUpdated: NSDate = NSDate()
+    
+    init(sessionType: SessionType, player: Player, opponent: Player, game: Game, id: String) {
+        self.sessionType = sessionType
+        self.player = player
+        self.opponent = opponent
+        self.game = game
+        self.id = id
+    }
+    
+    convenience init(sessionType: SessionType, player: Player, opponent: Player) {
+       let newID = player.playerID + opponent.playerID + NSDate().toString(formatString: "YYMMddhhmmss")
+        self.init(sessionType: sessionType, player: player, opponent: opponent, game: Game(), id: newID)
+       
+    }
     
     func makePlayerMove(atIndex index: Int) {
         if player.playerPiece == game.currentPlayer {
-            game.makeMoveAtIndex(index)
+            makeMove(atIndex: index)
         }
     }
     
     func makeOpponentMove(atIndex index: Int) {
         if opponent.playerPiece == game.currentPlayer {
-            game.makeMoveAtIndex(index)
+            makeMove(atIndex: index)
         }
     }
+    
+    func makeMove(atIndex index: Int) {
+        game.makeMoveAtIndex(index)
+//        currentPlayer = game.currentPlayer == player.playerPiece ? player : opponent
+    }
+    
+    /*func makeMove(#player: Player atIndex index: Int) {
+        
+    }*/
 }
 
 /**
-*  This extension provides custom initializers
+*  This extension provides the game status in a human-readable form
 */
 extension GameSession {
+    /**
+    This function returns the status of the game in a human-readable, friendly format.
     
-    /*init(sessionType: SessionType) {
-        let localID = NSDate().toString(formatString: "YYMMddssmm")
-        self.init(sessionType: .Local, player: Player(), opponent: nil, game: Game(), id: localID)
-    }*/
+    :returns: The status of the game as a String
+    */
+    func getHumanReadableStatus() -> String {
+        
+        var friendlyStatusString = ""
+        var friendlyState = ""
+        var friendlyName = ""
+        
+        // Assign the name
+        switch (game.winningState, sessionType) {
+            
+        case (.Tie, _):
+            friendlyName = "" // If it's a tie, we don't need a name
+            break
+        case (_, .Local):
+            friendlyName = "\(currentPlayer.playerPiece.description)" //TODO: Don't duplicate this...
+        default:
+            if currentPlayer.playerType == .Local {
+                friendlyName = "You"
+            } else {
+                friendlyName = currentPlayer.displayName
+            }
+        }
+        
+        // Assign the state of the game
+        switch (game.winningState, sessionType) {
+            
+        case (.Tie, _):
+            friendlyState = "It's a tie"
+        case (.Unwon, _):
+            if sessionType == .Local ||
+                (sessionType != .Local && currentPlayer.playerType != .Local) {
+                    
+                    friendlyState = "'s turn"
+            } else {
+                "r turn"
+            }
+        default:
+            friendlyState = " won!"
+        }
+        
+        friendlyStatusString = friendlyName + friendlyState
+        return friendlyStatusString
+    }
+   
 
 }
 
