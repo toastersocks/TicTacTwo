217f3ec7557303600d9918a02658f58340982fe1
James Pamplona <pamplonajames@gmail.com>
2015-03-06 15:16:34 -0800

    Interclass constants

---
+++ TicTacTwo!/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  TicTacTwo!
+//
+//  Created by James Pamplona on 3/4/15.
+//  Copyright (c) 2015 James Pamplona. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let localPlayer = "LocalPlayer"
+    static let localOpponent = "LocalOpponent"
+}
